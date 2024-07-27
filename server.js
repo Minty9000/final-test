a80cdfedf775866901e9442921c2255d34b3c64a
Minty9000 <gilbertoteng86@gmail.com>
2024-07-27 11:03:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const interval = 30000; // Interval in milliseconds (30 seconds)
 
 //Reloader Function
 function reloadWebsite() {
-  axios.get(url)
+  fetch(url)
     .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+      if (response.ok) {
+        console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+      } else {
+        throw new Error(`Status Code ${response.status}`);
+      }
     })
     .catch(error => {
       console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
